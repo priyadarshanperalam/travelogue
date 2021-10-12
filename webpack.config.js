ee4f4216ba3a42a5f440966acaf3f40da755bba9
PRIYADARSHAN PERALAM <PRIYADARSHAN@ONEPIXLAB.com>
2021-10-12 11:21:36 +0530

    build process complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,115 @@
-const path = require('path');
+const buildMode = process.env.npm_lifecycle_event;
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fse = require('fs-extra');
 
 const postCSSPlugins = [
-	require('postcss-import'),
-	require('postcss-mixins'),
-	require('postcss-simple-vars'),
-	require('postcss-nested'),
-	require('postcss-hexrgba'),
-	require('autoprefixer')
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("postcss-hexrgba"),
+  require("autoprefixer"),
 ];
 
-module.exports = {
-	entry: './app/assets/scripts/App.js',
-	output: {
-		filename: 'bundled.js',
-		path: path.resolve(__dirname,'app')
-	},
-	devServer:{
-		watchFiles: './app/**/*.html',
-		static: path.join(__dirname, 'app'),
-		hot: true,
-		port: 3000,
-		host:'0.0.0.0'
-	},
-	mode: 'development',
-	module: {
-		rules: [
-			{
-				test: /\.(css|jpg|png)$/i,
-				use: [
-					'style-loader', 
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								plugins: postCSSPlugins
-							}
-						}
-					}
-				]
+class RunAfterCompile {
+	apply(compiler) {
+		compiler.hooks.done.tap(
+			'Copy images',
+			function() {
+				fse.copySync('./app/assets/images', './docs/assets/images');
 			}
-		]
+		)
 	}
 }
+
+let cssConfig = {
+  test: /\.(css|jpg|png)$/i,
+  use: [
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: postCSSPlugins,
+        },
+      },
+    },
+  ],
+};
+
+//[new HtmlWebpackPlugin({filename: 'index.html', template: './app/index.html'})]
+
+let pages = fse
+  .readdirSync("./app")
+  .filter(function (file) {
+    return file.endsWith(".html");
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`,
+    });
+  });
+
+let config = {
+  entry: "./app/assets/scripts/App.js",
+  plugins: pages,
+  module: {
+    rules: [cssConfig],
+  },
+};
+
+if (buildMode == "dev") {
+  cssConfig.use.unshift("style-loader");
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  };
+  config.devServer = {
+    watchFiles: "./app/**/*.html",
+    static: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  };
+  config.mode = "development";
+}
+
+if (buildMode == "build") {
+	config.module.rules.push({
+		test: /\.js$/,
+		exclude: /(node_modules)/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env']
+			}
+		}
+	});
+
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+  postCSSPlugins.push(require("cssnano"));
+  config.output = {
+    filename: "[name].[chunkhash].bundled.js",
+    chunkFilename: "[name].[chunkhash].bundled.js",
+    path: path.resolve(__dirname, "docs"),
+  };
+  config.mode = "production";
+  config.optimization = {
+    splitChunks: { chunks: "all" },
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  };
+  config.plugins = [
+    ...config.plugins,
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "[name].[chunkhash].css" }),
+		new RunAfterCompile()
+  ];
+}
+
+module.exports = config;
